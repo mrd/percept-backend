32328c5278ae7c8f128076d6893ee3c77d2ca9bd
Matthew Danish <m.r.danish@uu.nl>
2023-03-18 14:21:08 +0100

    structure sql into migration

---
+++ migrations/20230317-schema/down.sql
@@ -0,0 +1,7 @@
+DROP TABLE survey;
+DROP TABLE person;
+DROP TABLE session;
+DROP TABLE cookie;
+DROP TABLE image;
+DROP TABLE category;
+DROP TABLE rating;
---
+++ migrations/20230317-schema/up.sql
@@ -0,0 +1,54 @@
+CREATE TABLE survey (
+    survey_id SERIAL PRIMARY KEY,
+    age SMALLINT,
+    monthly_gross_income VARCHAR(16),
+    education VARCHAR(64),
+    gender TEXT,
+    postalcode VARCHAR(12),
+    consent BOOLEAN
+);
+
+CREATE TABLE person (
+    person_id SERIAL PRIMARY KEY,
+    survey_id INTEGER REFERENCES survey
+);
+
+CREATE TABLE session (
+    session_id SERIAL PRIMARY KEY,
+    person_id INTEGER REFERENCES person,
+    session_start TIMESTAMPTZ DEFAULT now(),
+    session_active TIMESTAMPTZ DEFAULT now()
+);
+
+CREATE TABLE cookie (
+    cookie_hash BYTEA,
+    person_id INTEGER REFERENCES person,
+    cookie_creation TIMESTAMPTZ DEFAULT now(),
+    expiration TIMESTAMPTZ
+);
+
+CREATE TABLE image (
+    image_id SERIAL PRIMARY KEY,
+    system_path TEXT,
+    url TEXT,
+    cityname VARCHAR(24)
+);
+
+CREATE TABLE category (
+    category_id SERIAL PRIMARY KEY,
+    shortname VARCHAR(24)
+);
+
+CREATE TABLE rating (
+    ts TIMESTAMPTZ DEFAULT now(),
+    session_id INTEGER REFERENCES session,
+    image_id INTEGER REFERENCES image,
+    category_id INTEGER REFERENCES category,
+    rating SMALLINT
+);
+
+INSERT INTO category (shortname) VALUES ('walkability');
+INSERT INTO category (shortname) VALUES ('bikeability');
+INSERT INTO category (shortname) VALUES ('safety');
+INSERT INTO category (shortname) VALUES ('pleasantness');
+INSERT INTO category (shortname) VALUES ('greenness');
