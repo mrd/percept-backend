926c89f20e19301a2945d3254b473b0ce30a4fc8
Matthew Danish <m.r.danish@uu.nl>
2023-05-31 19:31:01 +0200

    remove redundant call

--- server.js
+++ server.js
@@ -240,8 +240,7 @@ async (req, res) => {
   }
 
   if (ts)
-    res.json({status: 'ok', timestamp: ts, ...await count_ratings_by_category(req.body),
-              ...await count_ratings_by_category(req.body)});
+    res.json({status: 'ok', timestamp: ts, ...await count_ratings_by_category(req.body)});
   else
     res.status(400).json({ errors: ['undo failed'] });
 });
